bc46bc892bbdc5bcb26dcb4f07e460b9fcf8395f
Fanny <fannyvr18@gmail.com>
2016-08-30 20:34:48 -0700

    refactor(profile): import correct files

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -7,37 +7,41 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE.txt file in the root directory of this source tree.
  */
-
 import React, { PropTypes } from 'react';
-import Layout from '../../components/Layout';
-import s from './styles.css';
 import { title, html } from './index.md';
+import Footer from '../../components/Footer';
+import Navigation from '../../components/Layout/Navigation';
+import Link from '../../components/Link';
+import s from '../../components/Layout/Header.css';
+import ss from './styles.css';
 
-class ProfilePage extends React.Component {
-
+class Header extends React.Component {
 
   componentDidMount() {
-    document.title = title;
+    window.componentHandler.upgradeElement(this.root);
+  }
+
+  componentWillUnmount() {
+    window.componentHandler.downgradeElements(this.root);
   }
 
   render() {
     return (
-      <Layout className={s.content}>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-        
-        <p>
-          <br /><br />
-          <br /><br />
-          <br /><br />
-          <br /><br />
-          <br /><br />
-          <br /><br />
-          <br /><br />
-        </p>
-      </Layout>
+      <div>
+      <header className={`mdl-layout__header ${s.header}`} ref={node => (this.root = node)}>
+        <div className={`mdl-layout__header-row ${s.row}`}>
+          <div className="mdl-layout-spacer"></div>
+          <Navigation />
+        </div>
+      </header>
+      <div dangerouslySetInnerHTML={{ __html: html }} />  
+        <Footer />
+      </div>
     );
   }
 
 }
 
-export default ProfilePage;
+export default Header;
+
+
